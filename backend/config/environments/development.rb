f8db4155e9461631d74db0d9e98c2b1c816ddc98
FukkaYY <nakayuyy04@gmail.com>
2026-09-01 17:02:50 +0900

    Windows Docker環境でRails APIを起動可能にする

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << "backend"
+  
   config.enable_reloading = true
   config.eager_load = false
   config.consider_all_requests_local = true
